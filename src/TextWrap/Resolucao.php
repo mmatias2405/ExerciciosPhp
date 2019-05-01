bcc4abc66362b16743a9e0d9929619ab46ea14e7
mmatias2405 <matheusmatias555@gmail.com>
2019-05-01 10:01:35 -0300

    Foi implementada a primeira versão da função textWarp

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -7,11 +7,65 @@ namespace Galoa\ExerciciosPhp\TextWrap;
  */
 class Resolucao implements TextWrapInterface {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function textWrap(string $text, int $length): array {
-    return [""];
-  }
-
+	/**
+	* {@inheritdoc}
+	*/
+    public function textWrap(string $text, int $length): array {
+        $ret = array();
+        $i=0;
+        while($i<strlen($text)){
+            $aux = "";
+            $lineSize = $length;
+            $breakLine = false;
+            $breakWord = false;
+            while(!$breakLine){
+                if($text[$i] != " "){
+                    for($j=$i;$j<strlen($text);$j++){
+                        if($text[$j] == " ")
+                            break;
+                    }
+                    $ws = $j-$i;
+                    if($ws<=$lineSize){
+                        if($aux != "")
+                            $aux.=" ";
+                        $last = $i+$ws-1;
+                        $frst = $i;
+                        for($j = $frst;$j<=$last;$j++){
+                            $aux.=$text[$j];
+                            $i++;
+                            $lineSize--;
+                        }
+                    }
+                    else{
+                        if($ws>$length)
+                            $breakWord = true;
+                        $breakLine = true;
+                    }
+                }
+                else{
+                    //$aux.= " ";
+                    $lineSize--;
+                    $i++;
+                }
+                if($breakWord){
+                    array_push($ret, $aux);
+                    $aux = "";
+                    $last = $i+$length-1;
+                    $frst = $i;
+                    for($j=$frst;$j<=$last;$j++){
+                         $aux.=$text[$j];
+                         $i++; 
+                     }
+                }
+                if($lineSize <= 0)
+                    $breakLine = true;
+                if($i>=strlen($text))
+                    $breakLine = true;
+            }
+            array_push($ret,$aux);
+        }
+        if(count($ret)==0)
+            return array("");
+        return $ret;
+    }
 }
